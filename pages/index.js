9fe5d4b02570b70a5a6a025849547e767ba21d0a
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-02-14 17:29:13 +0000

    dando cara pro login, criei a pasta styles e o arquivo de css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,53 @@
-function Home() {
-  return <h1>teste</h1>;
+// pages/index.js
+
+import React, { useState } from "react";
+import styles from "styles/LoginPage.module.css"; // Importa o arquivo de estilo CSS Module
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Nome de usuário:", username);
+    console.log("Senha:", password);
+    // Adicione aqui a lógica para autenticar o usuário
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <h2 className={styles.loginTitle}>Página de Login</h2>
+      <form onSubmit={handleSubmit} className={styles.loginForm}>
+        <div className={styles.inputGroup}>
+          <label htmlFor="username" className={styles.inputLabel}>
+            Nome de Usuário:
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            className={styles.inputField}
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="password" className={styles.inputLabel}>
+            Senha:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className={styles.inputField}
+          />
+        </div>
+        <button type="submit" className={styles.loginButton}>
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Home;
+export default LoginPage;
